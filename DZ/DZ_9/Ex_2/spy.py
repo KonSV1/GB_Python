e0eb6299886e94b3b778b0b33f300e60335e626f
KonSV1 <112967342+KonSV1@users.noreply.github.com>
2022-12-19 03:11:16 +0700

    edit log

--- DZ/DZ_9/Ex_2/spy.py
+++ DZ/DZ_9/Ex_2/spy.py
@@ -4,6 +4,6 @@ import time
 
 
 def log(update: Update, context: ContextTypes):
-    file  = open('DZ/DZ_9/db.csv','a', encoding='utf-8')
+    file  = open('DZ/DZ_9/Ex_2/db.csv','a', encoding='utf-8')
     file.write(f'{update.effective_user.first_name},{update.effective_user.id},{time.strftime("%m/%d/%Y, %H:%M:%S", time.localtime())},{update.message.text}\n')
     file.close()
