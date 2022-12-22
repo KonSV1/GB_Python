4177759bf460bac75d6e68e9e870dfafb1468685
KonSV1 <112967342+KonSV1@users.noreply.github.com>
2022-12-22 21:59:10 +0700

    Add Ex_2 DZ_9

--- DZ/DZ_9/Ex_2/bot_command.py
+++ DZ/DZ_9/Ex_2/bot_command.py
@@ -1,24 +1,59 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import time
-from spy import *
+import logging
 
 
 async def hi_comm(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    log(update, context)
-    await update.message.reply_text(f'Hi {update.effective_user.first_name}')
+    logging.info(f'Запрос --> {update.message.text}')
+    logging.info(f'Ответ --> Привет {update.effective_user.first_name}')
+    await update.message.reply_text(f'Привет {update.effective_user.first_name}')
+
 
 async def help_comm(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    log(update, context)
+    logging.info(f'Запрос --> {update.message.text}')
+    logging.info(f'Ответ --> /hi - приветствие\n/time - узнать дату и время\
+        \n/calc - калькулятор для 2-х чисел')
     await update.message.reply_text(f'/hi - приветствие\n/time - узнать дату и время\
-        \n/sum - посчитать сумму 2-х чисел ("/sum"_"число1"_"число2")')
+        \n/calc - калькулятор для 2-х чисел. Для получения справки по функции калькулятора наберите\n/helpCalc')
+
+
+async def help_calc(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logging.info(f'Запрос --> {update.message.text}')
+    logging.info(f'Ответ --> helpCalc (msg)')
+    await update.message.reply_text(f'Для проведения вычислений передайте строку вида:\
+        \n/calc (число-1) (число-2) (символ оперрации)\nn+ - сумма\n- - разность\n* - произведение\n/ - деление')
 
 async def time_comm(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    log(update, context)
+    logging.info(f'Запрос --> {update.message.text}')
+    logging.info(
+        f'Ответ --> {time.strftime("%m/%d/%Y, %H:%M:%S", time.localtime())}')
     await update.message.reply_text(f'{time.strftime("%m/%d/%Y, %H:%M:%S", time.localtime())}')
 
-async def sum_comm(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    log(update, context)
-    msg = (update.message.text).split()
-    x, y = int(msg[1]), int(msg[2]),
-    await update.message.reply_text(f'{x} + {y} = {x+y}')
+
+async def calc(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logging.info(f'Запрос --> {update.message.text}')
+    msg = (update.message.text).replace(",", ".")
+    msg = msg.split()
+    x, y, op = float(msg[1]), float(msg[2]), msg[3]
+    match op:
+        case '+':
+            res = round(x + y, 2)
+            logging.info(f'{x} {op} {y} = {res}')
+        case '-':
+            res = round(x - y, 2)
+            logging.info(f'{x} {op} {y} = {res}')
+        case '*':
+            res = round(x * y, 2)
+            logging.info(f'{x} {op} {y} = {res}')
+        case '/':
+            try:
+                res = round(x / y, 2)
+                logging.info(f'{x} {op} {y} = {res}')
+            except ZeroDivisionError:
+                res = 'на ноль делить нельзя'
+                logging.info(f'{x} {op} {y} = {res}')
+        case _:
+            res = 'неизвестная операция'
+            logging.info(f'{x} {op} {y} = {res}')
+    await update.message.reply_text(f'{x} {op} {y} = {res}')
--- DZ/DZ_9/Ex_2/main.py
+++ DZ/DZ_9/Ex_2/main.py
@@ -1,16 +1,25 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import bot_command as bc
 import os
+import logging
 
-path = 'token.config'
+import bot_command as bc
+
+ENC = 'utf-8'
+FORMAT_INFO = '%(asctime)s, %(module)s, %(funcName)s, %(name)s, --> %(message)s'
+
+logging.basicConfig(format=FORMAT_INFO, filename='DZ/DZ_9/Ex_2/log.txt',
+                    filemode='a', encoding=ENC, level=logging.INFO, datefmt='%m/%d/%Y %H:%M:%S')
+
+path = 'token.config'  # файл с Токеном
 d = open(path, 'r', encoding='utf-8')
 KEY = d.readline()
-app = ApplicationBuilder().token(KEY).build()
 
+app = ApplicationBuilder().token(KEY).build()
 app.add_handler(CommandHandler("hi", bc.hi_comm))
 app.add_handler(CommandHandler("time", bc.time_comm))
 app.add_handler(CommandHandler("help", bc.help_comm))
-app.add_handler(CommandHandler("sum", bc.sum_comm))
+app.add_handler(CommandHandler("helpCalc", bc.help_calc))
+app.add_handler(CommandHandler("calc", bc.calc))
 print('Server start')
 app.run_polling()
